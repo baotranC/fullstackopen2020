5399cc864b531d5cbea8563666fd20b85978e728
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-06-07 19:15:46 -0400

    Add Exercice 1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,7 +10,10 @@ const Header = ({value}) => (
 )
 
 const Statistic = ({text, value})=>(
-<div>{text} {value}</div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
 )
 
 const Statistics = ({good, neutral, bad, allClicks}) => {
@@ -53,18 +56,21 @@ const Statistics = ({good, neutral, bad, allClicks}) => {
 
   return (
     <div>
+      <table>
+      <tbody>
       <Statistic text='good' value={good}/>
       <Statistic text='neutral' value={neutral}/>
       <Statistic text='bad' value={bad}/>  
       <Statistic text='all' value={sum()}/>
       <Statistic text='average' value={average()}/>
       <Statistic text='positive' value={percentPositive()+' %'}/>
+      </tbody>
+      </table>
     </div>
   )
 }
 
 const App = () => {
-  // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
