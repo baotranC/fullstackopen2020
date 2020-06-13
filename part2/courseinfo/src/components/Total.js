8231120646f8774d840d00ec1ba8227d0987369a
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-06-13 00:07:44 -0400

    Add exercice 2.3

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises + course.parts[3].exercises
+const Total = (props) => {
+
+  const parts = props.course.parts
+
+  const total = parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+
   return(
-    <b>Total of {sum} exercises</b>
-  ) 
+    <b>Total of {total} exercises</b>
+  )
 }
 
 export default Total
