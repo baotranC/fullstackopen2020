137cb03965ccb67f7f841d304b5483423b95623c
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-06-16 11:41:58 -0400

    Add exercice 2.4 and 2.5

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -4,11 +4,11 @@ import Part from './Part'
 const Content = ({ course }) => {
   const parts = course.parts
 
-    return (
-      <div>
-        {parts.map(part => <Part key = {part.id} part = {part}/>)}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {parts.map(part => <Part key = {part.id} part = {part}/>)}
+    </div>
+  )
+}
 
   export default Content
