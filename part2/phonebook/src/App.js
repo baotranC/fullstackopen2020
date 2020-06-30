6c1b2c26d7dbd50d2acc727e7338c6682a2ef933
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-06-30 18:40:52 -0400

    Add exercice 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Person from './components/Person'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-  
+  // const [ persons, setPersons ] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456' },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523' },
+  //   { name: 'Dan Abramov', number: '12-43-234345' },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  // ]) 
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
-  
-  const contactToShow = (newFilter.trim() === '') ? persons : persons.filter(person => person.name.toLowerCase().includes(newFilter.trim().toLowerCase())) 
 
+  const hook = () => {
+    const eventHandler = response => {
+      setPersons(response.data)
+    }
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }
+  useEffect(hook, [])
+
+  const contactToShow = (newFilter.trim() === '') ? persons : persons.filter(person => person.name.toLowerCase().includes(newFilter.trim().toLowerCase())) 
+  
   const addContact = (event) =>{
     event.preventDefault()
 
@@ -37,6 +47,7 @@ const App = () => {
 
       setPersons(persons.concat(person))
       setNewName('')
+      setNewNumber('')
     }
   }
 
